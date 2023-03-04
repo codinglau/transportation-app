286d14e52244b54a4febd050db1a8c1a584c8416
codinglau <codinglau@gmail.com>
2023-03-05 02:12:15 +0800

    update: set public path to /transportation-app

--- quasar.config.js
+++ quasar.config.js
@@ -68,7 +68,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/transportation-app',
       // analyze: true,
       env: {
         API: {
@@ -101,7 +101,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
-      // https: true
+      https: true,
       open: true // opens browser window automatically
     },
 
